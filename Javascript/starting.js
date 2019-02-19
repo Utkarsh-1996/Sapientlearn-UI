a3362d45bb4f92d5a641a6823b818c56f69ac554
Kumar Utkarsha <kumutkar@publicisgroupe.net>
2019-02-19 13:13:21 +0530

    day 7 part1

---
+++ Javascript/starting.js
@@ -0,0 +1,28 @@
+console.log('hello');
+var x=10;
+var name='utkarsh';
+var users=['john','kieth']
+
+// if(typeof(x)=='number'){
+//     document.write('x is a number');
+// }
+// else{
+//     document.write('its not');
+// }
+// for(var i=1;i<=10;i++){
+//     document.write(i);
+// }
+// document.write('<br>'+i)
+function clickme(){
+    window.alert('hi');
+    var n=10;
+}
+// document.write(n)
+// console.log(n)
+//console.log(window.document);
+
+// document.getElementById();
+// document.getElementsByClassName();
+// document.getElementsByTagName();
+var b1=document.getElementsByTagName('button');
+console.log(b1);
---
+++ bootstrap/register.js
@@ -0,0 +1,8 @@
+uname=document.getElementById('usertext');
+pwd=document.getElementById('passtext');
+email=document.getElementById('emailtext');
+function reguser(){
+    console.log(name.value);
+    console.log(passtext.value);
+    console.log(emailtext.value);
+}
