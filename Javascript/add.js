0b3a670f3c9aec83846c81818a0864a6e37ad963
Kumar Utkarsha <kumutkar@publicisgroupe.net>
2019-02-22 13:02:24 +0530

    modification

--- Javascript/add.js
+++ Javascript/add.js
@@ -11,14 +11,23 @@ var x=document.getElementById("a");
 // function createElement(type,id){
 
 // }
+var myPosts=[{text:"",time:""}]
 function createpost(){
-    var postContent=document.getElementById("mypost");
+    var postContent=document.getElementById("myPost");
+    var postContainer=document.getElementById("postContainer");
+    postContainer.innerHTML="";
+    myPosts.push({
+        text: postContent.value,
+        time: new Date()
+    });
+    for(var i=myPosts.length-1;i>=0;i--){
     var myCard=`<div class="card">
                     <div class="card-body">
-                        ${postContent.value}
+                        ${myPosts[i].text}${myPosts[i].time}
                     </div>
                 </div>`;
-    postContainer.innerHTML=myCard;
+    postContainer.innerHTML+=myCard;
+    }
     postContent.value="";
 }
 
